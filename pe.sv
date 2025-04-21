bdc610f67377228ac50bf0e8c996f524ad6cd402
poyraz güler <148434875+poytoy@users.noreply.github.com>
2025-04-21 16:48:51 +0300

    Update pe.sv

--- pe.sv
+++ pe.sv
@@ -9,14 +9,18 @@ module pe (
     output logic [31:0] psum_out     // output partial sum
 );
  //IMPLEMENT
-//determining the next psum_out 
+//keep psum_in for one more cycle
+logic [31:0] psum_in_saved;
+always_ff @(posedge clk or posedge rst) begin
+            psum_in_saved <= psum_in;
+            end 
 always_ff @(posedge clk or posedge rst) begin
         if (rst) begin
             psum_out <= 0;
         end else if (image_en && weight_en) begin
-            psum_out <= psum_in + image_val * weight_val;
+            psum_out <= psum_in_saved + image_val * weight_val;
         end else begin
-            psum_out <= psum_in; //  Forward directly
+            psum_out <= psum_in; // ⬅️ Forward directly
         end
     end
 always_ff @(posedge clk or posedge rst) begin
@@ -27,7 +31,7 @@ always_ff @(posedge clk or posedge rst) begin
               $time, image_val, weight_val, psum_in, image_val * weight_val + psum_in,
              image_en, weight_en);
 end else if (psum_in != 0 || psum_out != 0) begin
-    $display(" @%0t | Forward: psum_in=%0d → psum_out=%0d",
+    $display(" @%0t | Forward: psum_in=%0d  psum_out=%0d",
               $time, psum_in, psum_out);
 end
 end 
